cb613641eac5e14dcd6171a2f0aa77730bd0fa5e
Pierre-Yves Caneill <pyc@moeryn.com>
2015-11-13 20:18:57 +0100

    Adding a cleaning tool in order to test the build

---
+++ clean.sh
@@ -0,0 +1,24 @@
+# TODO: use function
+#function clean_directory()
+#{
+#    rm -rf ./CMakeFiles
+#    rm -rf ./CMakeCache.txt
+#    rm -rf ./cmake_install.cmake
+#    rm -rf Makefile
+#}
+
+make clean
+rm -rf ./CMakeFiles
+rm -rf ./CMakeCache.txt
+rm -rf ./cmake_install.cmake
+rm -rf Makefile
+cd src
+rm -rf ./CMakeFiles
+rm -rf ./CMakeCache.txt
+rm -rf ./cmake_install.cmake
+rm -rf Makefile
+cd test
+rm -rf ./CMakeFiles
+rm -rf ./CMakeCache.txt
+rm -rf ./cmake_install.cmake
+rm -rf Makefile
