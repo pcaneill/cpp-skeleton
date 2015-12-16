661341afa9410d9afc3653380da6b724abd0dcf0
Pierre-Yves Caneill <pyc@moeryn.com>
2015-12-16 16:47:41 +0100

    Update the tests

--- build_tests/normal.sh
+++ build_tests/normal.sh
@@ -6,24 +6,24 @@ make P=$1
 make P=$1 test
 
 # Run the tests manually
-cd build/$1
-./src/lib1/test/lib1_test
-./src/lib2/test/lib2_test
+./build/$1/src/lib1/test/lib1_test
+./build/$1/src/lib2/test/lib2_test
 
-# Touch a file to test incremental build
-cd ../../src/lib1/
-touch main.cpp
-
-# Try to compile
 # TODO: find a way to check that the file has been compiled
-cd ../../build/$1/src/lib1
+# Touch a file to test incremental build
+cd src/lib1/
+touch main.cpp
 make P=$1
 make P=$1 test
 
 # Touch the test file to check incremental build, compile & run tests
-cd ../../../../src/lib1/test
+cd test/
 touch test.cpp
-cd ../../../build/$1/src/lib1/test
 make P=$1
 make P=$1 test
-./lib1_test
+
+cd ../../../
+
+# Run the tests manually
+./build/$1/src/lib1/test/lib1_test
+./build/$1/src/lib2/test/lib2_test
