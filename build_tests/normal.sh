bb3fe1fc42ca73929c8c39a90340c43102bef558
Pierre-Yves Caneill <pyc@moeryn.com>
2016-07-01 17:24:27 +0200

    Repair the tests

--- build_tests/normal.sh
+++ build_tests/normal.sh
@@ -6,13 +6,13 @@ make P=$1
 make P=$1 test
 
 # Run the tests manually
-./.build/$1/src/lib1/test/lib1_test
-./.build/$1/src/lib2/test/lib2_test
+./src/lib1/test/ctest
+./src/lib2/test/ctest
 
 # TODO: find a way to check that the file has been compiled
 # Touch a file to test incremental build
 cd src/lib1/
-touch main.cpp
+touch sub1/sub1.cpp
 make P=$1
 make P=$1 test
 
@@ -24,6 +24,7 @@ make P=$1 test
 
 cd ../../../
 
-# Run the tests manually
-./.build/$1/src/lib1/test/lib1_test
-./.build/$1/src/lib2/test/lib2_test
+touch src/lib1/include/lib1/sub1/public_sub1.hpp
+make P=$1
+
+make distclean
