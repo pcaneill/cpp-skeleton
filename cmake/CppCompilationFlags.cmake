95309cbb35fa0fdb6da6366246ffab6d97a36cd3
Pierre-Yves Caneill <pyc@moeryn.com>
2016-07-03 18:41:03 +0200

    Move compilation Options to CppCompilationFlags

---
+++ cmake/CppCompilationFlags.cmake
@@ -0,0 +1,42 @@
+add_compile_options (
+ -std=c++14
+# Warnings
+ -Wall
+ -Wextra
+ -Werror
+ -Wcast-align
+ -Wcast-qual
+ -Wchar-subscripts
+ -Wenum-compare
+ -Wformat-nonliteral
+ -Wmissing-format-attribute
+ -Winit-self
+ -Wmissing-declarations
+ -Wmissing-include-dirs
+ -Wno-error=deprecated-declarations
+ -Wno-format-y2k
+ -Wno-format-zero-length
+ -Wno-unused-parameter
+ -Wparentheses
+ -Wpointer-arith
+ -Wredundant-decls
+ -Wsequence-point
+ -Wshadow
+ -Wsign-compare
+ -Wswitch-default
+ -Wuninitialized
+ -Wunused
+ -Wwrite-strings
+# Auto vectorization
+ -ftree-vectorize
+# Formating
+ -fshow-column
+ -fdiagnostics-show-option
+ -fdiagnostics-color=always
+ -funsigned-char
+ -fno-strict-aliasing
+# Code Generation Convention
+ -fwrapv
+
+ -pedantic
+ )
